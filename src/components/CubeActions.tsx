c01f13d599cdcda5c4534a34bd214d027a03c458
RahimUddinMsc <rahim_uddin@live.co.uk>
2024-04-08 15:30:27 +0100

    movement functionality complete, functions moved to custom hooks with ui updated

--- src/components/CubeActions.tsx
+++ src/components/CubeActions.tsx
@@ -1,30 +1,29 @@
 import Direction from '../constants/movementDirection';
 import useCubeActions from '../hooks/useCubeActions';
-import IMovementProps from '../interfaces/IMovementActions';
-import { rotateFront, rotateHorizontal, rotateRight } from '../lib/arrayUtils';
+import IMovementProps from '../interfaces/IMovementProps';
 import actionStyle from './../css/cubeAction.module.css'
 
-const CubeMovementActions = ( prop: IMovementProps) => {
-    const rubiks = useCubeActions()
+const CubeMovementActions = (prop: IMovementProps) => {
+    const action = useCubeActions(prop)
 
     return <>
         <div className={actionStyle.action_container}>
             <div className={actionStyle.middle_action_container}>
                 <div className={actionStyle.middle_clockwise_actions}>
-                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-front" onClick={() => prop.setCubeData(rotateFront(prop.cubeData, Direction.CLOCKWISE))}>F</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-right" onClick={() => prop.setCubeData(rotateRight(prop.cubeData, Direction.CLOCKWISE))}>R</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-up" onClick={() => prop.setCubeData(rotateHorizontal(prop.cubeData, Direction.CLOCKWISE))}>U</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-bottom">B</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-left">L</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-down">D</button>                    
+                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-front" onClick={() => action.rotateRubikFront(Direction.CLOCKWISE)}>F</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-right" onClick={() => action.rotateRubikRight(Direction.CLOCKWISE)}>R</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-up" onClick={() => action.rotateRubikUp(Direction.CLOCKWISE)}>U</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-bottom" onClick={() => action.rotateRubikBack(Direction.CLOCKWISE)}>B</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-left" onClick={() => action.rotateRubikLeft(Direction.CLOCKWISE)}>L</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="clockwise-down" onClick={() => action.rotateRubikDown(Direction.CLOCKWISE)}>D</button>                    
                 </div>
                 <div className={actionStyle.middle_anticlockwise_actions}>
-                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-front" onClick={() => prop.setCubeData(rotateFront(prop.cubeData, Direction.ANTICLOCKWISE))}>F'</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-right" onClick={() => prop.setCubeData(rotateRight(prop.cubeData, Direction.ANTICLOCKWISE))}>R'</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-up" onClick={() => prop.setCubeData(rotateHorizontal(prop.cubeData, Direction.ANTICLOCKWISE))}>U'</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-bottom">B'</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-left">L'</button>
-                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-down">D'</button>                    
+                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-front" onClick={() => action.rotateRubikFront(Direction.ANTICLOCKWISE)}>F'</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-right" onClick={() => action.rotateRubikRight(Direction.ANTICLOCKWISE)}>R'</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-up" onClick={() => action.rotateRubikUp(Direction.ANTICLOCKWISE)}>U'</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-bottom" onClick={() => action.rotateRubikBack(Direction.ANTICLOCKWISE)}>B'</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-left" onClick={() => action.rotateRubikLeft(Direction.ANTICLOCKWISE)}>L'</button>
+                    <button className={actionStyle.movement_action_btn} data-direction="anitclockwise-down" onClick={() => action.rotateRubikDown(Direction.ANTICLOCKWISE)}>D'</button>                    
                 </div>
             </div>            
         </div>    
--- src/hooks/useCubeActions.ts
+++ src/hooks/useCubeActions.ts
@@ -1,5 +1,157 @@
-const useCubeActions = () => {
+import Direction from "../constants/movementDirection";
+import IMovementProps from "../interfaces/IMovementProps";
+import IRubikCube from "../interfaces/IRubikCube";
+import { replaceMultiDimensionArrayVals, rotateMultiDimensionArray } from "../lib/arrayUtils";
+
+const useCubeActions = (action: IMovementProps) => {
     
+    const rotateRubikFront = (direction: Direction) => {
+        action.setCubeData(rotateFront(action.cubeData, direction))        
+    }
+
+    const rotateRubikRight = (direction: Direction) => {
+        action.setCubeData(rotateRight(action.cubeData, direction))
+    }
+
+    const rotateRubikUp = (direction: Direction) => {
+        action.setCubeData(rotateUp(action.cubeData, direction))
+    }
+
+    const rotateRubikBack = (direction: Direction) => {
+        action.setCubeData(rotateBack(action.cubeData, direction))
+    }
+
+    const rotateRubikLeft = (direction: Direction) => {
+        action.setCubeData(rotateLeft(action.cubeData, direction))
+    }
+
+    const rotateRubikDown = (direction: Direction) => {
+        action.setCubeData(rotateDown(action.cubeData, direction))
+    }
+
+    return {rotateRubikFront, rotateRubikRight, rotateRubikUp, rotateRubikBack, rotateRubikLeft, rotateRubikDown}
+
 }
+
+export const rotateFront = (rubikCube: IRubikCube, direction: Direction) => {    
+    let updatedCube = JSON.parse(JSON.stringify(rubikCube));
+
+    if(direction === Direction.CLOCKWISE){
+        updatedCube.front = rotateMultiDimensionArray(updatedCube.front, direction);
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.left, updatedCube.top, [{row:2, index:0}, {row:2, index:1}, {row:2, index:2}], [{row:2, index:2}, {row:1, index:2}, {row:0, index:2}]);       
+        updatedCube.right = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.right, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:2, index:0}, {row:2, index:1}, {row:2, index:2}]);
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.right, updatedCube.bottom, [{row:0, index:0}, {row:0, index:1}, {row:0, index:2}], [{row:2, index:0}, {row:1, index:0}, {row:0, index:0}]);
+        updatedCube.left = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.left, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:0, index:0},{row:0, index:1}, {row:0, index:2}]);        
+    }else{        
+        updatedCube.front = rotateMultiDimensionArray(updatedCube.front, direction)                            
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.right, updatedCube.top, [{row:2, index:0}, {row:2, index:1}, {row:2, index:2}], [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}]);       
+        updatedCube.left = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.left, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:2, index:2}, {row:2, index:1}, {row:2, index:0}]);
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.left, updatedCube.bottom, [{row:0, index:0}, {row:0, index:1}, {row:0, index:2}], [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}]);
+        updatedCube.right = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.right, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:0, index:2},{row:0, index:1}, {row:0, index:0}]);        
+    }
+
+    return updatedCube;
+}
+
+export const rotateRight = (rubikCube: IRubikCube, direction: Direction) => {    
+    let updatedCube = JSON.parse(JSON.stringify(rubikCube));    
+
+    if(direction === Direction.CLOCKWISE){
+        updatedCube.right = rotateMultiDimensionArray(updatedCube.right, direction);
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.front, updatedCube.top, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}]);       
+        updatedCube.back = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.back, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:2, index:2},{row:1, index:2}, {row:0, index:2}]);        
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.back, updatedCube.bottom, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:2, index:0}, {row:1, index:0}, {row:0, index:0}]);        
+        updatedCube.front = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.front, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}]);                 
+    }else{        
+        updatedCube.right = rotateMultiDimensionArray(updatedCube.right, direction);
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.back, updatedCube.top, [{row:0, index:2},{row:1, index:2},{row:2, index:2}], [{row:2, index:0},{row:1, index:0},{row:0, index:0}]);                
+        updatedCube.front = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.front, [{row:0, index:2},{row:1, index:2},{row:2, index:2}], [{row:0, index:2},{row:1, index:2},{row:2, index:2}]);                
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.front, updatedCube.bottom, [{row:0, index:2},{row:1, index:2},{row:2, index:2}], [{row:0, index:2},{row:1, index:2},{row:2, index:2}]);        
+        updatedCube.back = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.back, [{row:0, index:0},{row:1, index:0},{row:2, index:0}], [{row:2, index:2},{row:1, index:2},{row:0, index:2}]);        
+    }
+
+    return updatedCube;
+}
+
+export const rotateUp = (rubikCube: IRubikCube, direction: Direction) => {    
+    let updatedCube = JSON.parse(JSON.stringify(rubikCube));    
+
+    if(direction === Direction.CLOCKWISE){
+        updatedCube.top = rotateMultiDimensionArray(updatedCube.top, direction);          
+        updatedCube.front[0] = rubikCube.right[0];
+        updatedCube.left[0] = rubikCube.front[0];
+        updatedCube.back[0] = rubikCube.left[0];
+        updatedCube.right[0] = rubikCube.back[0];        
+    }else{        
+        updatedCube.top = rotateMultiDimensionArray(updatedCube.top, direction); 
+        updatedCube.front[0] = rubikCube.left[0];
+        updatedCube.right[0] = rubikCube.front[0];
+        updatedCube.back[0] = rubikCube.right[0];
+        updatedCube.left[0] = rubikCube.back[0];        
+    }
+
+    return updatedCube;
+}
+
+export const rotateBack = (rubikCube: IRubikCube, direction: Direction) => {    
+    let updatedCube = JSON.parse(JSON.stringify(rubikCube));    
+
+    if(direction === Direction.CLOCKWISE){
+        updatedCube.back = rotateMultiDimensionArray(updatedCube.back, direction);
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.right, updatedCube.top, [{row:0, index:0}, {row:0, index:1}, {row:0, index:2}], [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}]);       
+        updatedCube.left = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.left, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:0, index:2},{row:0, index:1}, {row:0, index:0}]);        
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.left, updatedCube.bottom, [{row:2, index:0}, {row:2, index:1}, {row:2, index:2}], [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}]);        
+        updatedCube.right = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.right, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:2, index:2}, {row:2, index:1}, {row:2, index:0}]);                 
+    }else{        
+        updatedCube.back = rotateMultiDimensionArray(updatedCube.back, direction);
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.left, updatedCube.top, [{row:0, index:0},{row:0, index:1},{row:0, index:2}], [{row:2, index:0},{row:1, index:0},{row:0, index:0}]);                
+        updatedCube.right = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.right, [{row:0, index:2},{row:1, index:2},{row:2, index:2}], [{row:0, index:0},{row:0, index:1},{row:0, index:2}]);                
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.right, updatedCube.bottom, [{row:2, index:0},{row:2, index:1},{row:2, index:2}], [{row:2, index:2},{row:1, index:2},{row:0, index:2}]);        
+        updatedCube.left = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.left, [{row:0, index:0},{row:1, index:0},{row:2, index:0}], [{row:2, index:0},{row:2, index:1},{row:2, index:2}]);        
+    }
+
+    return updatedCube;
+}
+
+export const rotateLeft = (rubikCube: IRubikCube, direction: Direction) => {    
+    let updatedCube = JSON.parse(JSON.stringify(rubikCube));    
+
+    if(direction === Direction.CLOCKWISE){
+        updatedCube.left = rotateMultiDimensionArray(updatedCube.left, direction);
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.back, updatedCube.top, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:2, index:2}, {row:1, index:2}, {row:0, index:2}]);
+        updatedCube.front = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.front, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}]);
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.front, updatedCube.bottom, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:0, index:0},{row:1, index:0}, {row:2, index:0}]);
+        updatedCube.back = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.back, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:2, index:0}, {row:1, index:0}, {row:0, index:0}]);              
+    }else{        
+        updatedCube.left = rotateMultiDimensionArray(updatedCube.left, direction);        
+        updatedCube.top = replaceMultiDimensionArrayVals(rubikCube.front, updatedCube.top, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}]);
+        updatedCube.back = replaceMultiDimensionArrayVals(rubikCube.top, updatedCube.back, [{row:0, index:2}, {row:1, index:2}, {row:2, index:2}], [{row:2, index:0}, {row:1, index:0}, {row:0, index:0}]);
+        updatedCube.bottom = replaceMultiDimensionArrayVals(rubikCube.back, updatedCube.bottom, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:2, index:2},{row:1, index:2}, {row:0, index:2}]);
+        updatedCube.front = replaceMultiDimensionArrayVals(rubikCube.bottom, updatedCube.front, [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}], [{row:0, index:0}, {row:1, index:0}, {row:2, index:0}]);
+    }
+
+    return updatedCube;
+}
+
+export const rotateDown = (rubikCube: IRubikCube, direction: Direction) => {    
+    let updatedCube = JSON.parse(JSON.stringify(rubikCube));    
+
+    if(direction === Direction.CLOCKWISE){
+        updatedCube.bottom = rotateMultiDimensionArray(updatedCube.bottom, direction);
+        updatedCube.front[2] = rubikCube.left[2];
+        updatedCube.right[2] = rubikCube.front[2];
+        updatedCube.back[2] = rubikCube.right[2];
+        updatedCube.left[2] = rubikCube.back[2];        
+    }else{        
+        updatedCube.bottom = rotateMultiDimensionArray(updatedCube.bottom, direction);
+        updatedCube.front[2] = rubikCube.right[2];
+        updatedCube.left[2] = rubikCube.front[2];
+        updatedCube.back[2] = rubikCube.left[2];
+        updatedCube.right[2] = rubikCube.back[2];            
+    }
+
+    return updatedCube;
+}
+
 
 export default useCubeActions;
