56a7dcc761d3cfdfb70782673eb8978a80443e76
RahimUddinMsc <rahim_uddin@live.co.uk>
2024-05-12 20:11:13 +0100

    Rotation functionality complete

--- src/components/DefaultCube.tsx
+++ src/components/DefaultCube.tsx
@@ -1,27 +1,35 @@
+import Direction from "../constants/movementDirection";
 import useDefaultCubeView from "../hooks/useDefaultCubeView";
 import IRubikCube from "../interfaces/IRubikCube";
 import style from './../css/defaultCube.module.css';
 import CubePanels from "./CubePanels";
+import './../css/defaultCube.module.css';
 
 const DefaultCube = ({rubikCube}: {rubikCube: IRubikCube}) => {
     const rubikHandler = useDefaultCubeView(rubikCube);
+    const rubikViewHandler = rubikHandler.view();
 
     return <>    
+        <button onClick={() => rubikHandler.rotateCube(Direction.ANTICLOCKWISE)}>left</button>
+
         <div id="default_cube_container" className={style.default_cube_container}>
             <div className={style.default_cube_modifier}>
-                <div className={style.default_cube_top}>
-                    <CubePanels rubikPanels={rubikCube.top.flat()}/>            
+                <div className={`${style.default_cube_top} ${style[rubikViewHandler!.topViewRotation]}`}>
+                    <CubePanels rubikPanels={rubikHandler.flipped ? rubikCube.bottom.flat().reverse() : rubikCube.top.flat()}/>            
                 </div>
 
                 <div className={style.default_cube_front}>
-                    <CubePanels rubikPanels={rubikHandler.view.front.flat()}/>            
+                    <CubePanels rubikPanels={rubikHandler.flipped ? rubikViewHandler!.front.flat().reverse() : rubikViewHandler!.front.flat()}/>            
                 </div>
 
                 <div className={style.default_cube_right}>
-                    <CubePanels rubikPanels={rubikCube.right.flat()}/>            
+                    <CubePanels rubikPanels={rubikHandler.flipped ? rubikViewHandler!.right.flat().reverse() : rubikViewHandler!.right.flat()}/>            
                 </div>            
             </div>            
         </div>        
+
+        <button onClick={() => rubikHandler.rotateCube(Direction.CLOCKWISE)}>right</button>
+        <button onClick={() => rubikHandler.setFlipped(!rubikHandler.flipped)}>flips</button>
     </>
 }
 
--- src/hooks/useDefaultCubeView.ts
+++ src/hooks/useDefaultCubeView.ts
@@ -4,32 +4,32 @@ import Direction from "../constants/movementDirection";
 
 const useDefaultCubeView = (rubikCube: IRubikCube) => {
     const [viewIndex, setViewIndex] = useState(0);
+    const [flipped, setFlipped] = useState(false);
 
     const rotateCube = (direction: Direction) => {
         let currentIndex = viewIndex;
 
-        Direction.CLOCKWISE ? currentIndex++ : currentIndex--
+        direction === Direction.CLOCKWISE ? currentIndex-- : currentIndex++
 
-        if(currentIndex == 4)
+        if(currentIndex === 4)
             currentIndex = 0
 
-        if(currentIndex == -1)
-            currentIndex = 4
+        if(currentIndex === -1)
+            currentIndex = 3
 
         setViewIndex(currentIndex)
     }
 
     const rubikViewMap = new Map([
-        [0, {front: rubikCube.front, left: rubikCube.right}],
-        [1, {front: rubikCube.right, left: rubikCube.back}],
-        [2, {front: rubikCube.back, left: rubikCube.left}],
-        [3, {front: rubikCube.left, left: rubikCube.front}]
+        [0, {front: rubikCube.front, right: flipped ? rubikCube.left : rubikCube.right , topViewRotation: 'default_cube_top_rotated_right'}],
+        [1, {front: flipped ? rubikCube.left : rubikCube.right, right: rubikCube.back, topViewRotation: 'default_cube_top_rotated_left'}],
+        [2, {front: rubikCube.back, right: flipped ? rubikCube.right : rubikCube.left, topViewRotation: 'default_cube_top_rotated_top'}],
+        [3, {front: flipped ? rubikCube.right : rubikCube.left, right: rubikCube.front, topViewRotation: 'default_cube_top_rotated_back'}]
     ]);
 
     const view = () => rubikViewMap.get(viewIndex);
 
-    return {rotateCube, view}
-
+    return {rotateCube, view, flipped, setFlipped}
 }
 
 export default useDefaultCubeView;
